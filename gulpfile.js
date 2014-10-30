10de6cf5d16009fe94ec64e2f1680250dae20240
Frank Wang <frank.wangtao@gmail.com>
2014-10-30 15:31:42 -0400

    Disable browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
 var watch = require('gulp-watch');
 
 gulp.task('default', function() {
@@ -13,21 +11,15 @@ gulp.task('sass', function() {
     return gulp.src('scss/*.scss')
 	.pipe(sass())
 	.pipe(gulp.dest('./'))
-	.pipe(reload({stream: true}));
 });
 
 gulp.task('serve', function() {
-    browserSync({
-	proxy: 'localhost:8888'
-    });
 
     watch('**/*.scss', function() {
 	gulp.start('sass');
     })
 
     watch('js/**/*.js')
-	.pipe(reload({stream: true}));
 
     watch('**/*.php')
-	.pipe(reload({stream: true}));
 });
