7d332928337194f85b6a9549ef457a4b7c317131
Frank Wang <frank.wangtao@gmail.com>
2014-10-31 12:24:20 -0400

    Change dropdown animation, add logo triangle

--- functions.php
+++ functions.php
@@ -4,72 +4,72 @@
 
 if ( ! function_exists( 'theme_setup' ) ):
 
-function theme_setup() {
+    function theme_setup() {
 
-	/* This theme uses post thumbnails (aka "featured images")
-	*  all images will be cropped to thumbnail size (below), as well as
-	*  a square size (also below). You can add more of your own crop
-	*  sizes with add_image_size. */
-	add_theme_support( 'post-thumbnails' );
-	set_post_thumbnail_size(120, 90, true);
-	add_image_size('square', 150, 150, true);
+        /* This theme uses post thumbnails (aka "featured images")
+         *  all images will be cropped to thumbnail size (below), as well as
+         *  a square size (also below). You can add more of your own crop
+         *  sizes with add_image_size. */
+        add_theme_support( 'post-thumbnails' );
+        set_post_thumbnail_size(120, 90, true);
+        add_image_size('square', 150, 150, true);
 
 
-	// Add default posts and comments RSS feed links to head
-	add_theme_support( 'automatic-feed-links' );
+        // Add default posts and comments RSS feed links to head
+        add_theme_support( 'automatic-feed-links' );
 
-	/* This theme uses wp_nav_menu() in one location.
-	* You can allow clients to create multiple menus by
-  * adding additional menus to the array. */
-	register_nav_menus( array(
-		'primary' => 'Primary Navigation'
-	) );
+        /* This theme uses wp_nav_menu() in one location.
+         * You can allow clients to create multiple menus by
+         * adding additional menus to the array. */
+        register_nav_menus( array(
+            'primary' => 'Primary Navigation'
+        ) );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
-	) );
+        /*
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support( 'html5', array(
+            'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+        ) );
 
-}
+    }
 endif;
 
 add_action( 'after_setup_theme', 'theme_setup' );
 
 
 /* Add all our JavaScript files here.
-We'll let WordPress add them to our templates automatically instead
-of writing our own script tags in the header and footer. */
+   We'll let WordPress add them to our templates automatically instead
+   of writing our own script tags in the header and footer. */
 
 function hackeryou_scripts() {
 
 	//Don't use WordPress' local copy of jquery, load our own version from a CDN instead
 	wp_deregister_script('jquery');
-  wp_enqueue_script(
-  	'jquery',
-  	"http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js",
-  	false, //dependencies
-  	null, //version number
-  	true //load in footer
-  );
+    wp_enqueue_script(
+        'jquery',
+        "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js",
+        false, //dependencies
+        null, //version number
+        true //load in footer
+    );
 
-  wp_enqueue_script(
-    'plugins', //handle
-    get_template_directory_uri() . '/js/plugins.js', //source
-    false, //dependencies
-    null, // version number
-    true //load in footer
-  );
+    wp_enqueue_script(
+        'plugins', //handle
+        get_template_directory_uri() . '/js/plugins.js', //source
+        false, //dependencies
+        null, // version number
+        true //load in footer
+    );
 
-  wp_enqueue_script(
-    'scripts', //handle
-    get_template_directory_uri() . '/js/scripts.js', //source
-    array( 'jquery', 'plugins' ), //dependencies
-    null, // version number
-    true //load in footer
-  );
+    wp_enqueue_script(
+        'scripts', //handle
+        get_template_directory_uri() . '/js/scripts.js', //source
+        array( 'jquery', 'plugins' ), //dependencies
+        null, // version number
+        true //load in footer
+    );
 }
 
 add_action( 'wp_enqueue_scripts', 'hackeryou_scripts' );
@@ -104,7 +104,7 @@ add_filter( 'wp_title', 'hackeryou_wp_title', 10, 2 );
 
 /*
   Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
- */
+*/
 function hackeryou_page_menu_args( $args ) {
 	$args['show_home'] = true;
 	return $args;
@@ -195,46 +195,46 @@ if ( ! function_exists( 'hackeryou_posted_on' ) ) :
 /**
  * Prints HTML with meta information for the current post—date/time and author.
  */
-function hackeryou_posted_on() {
-	printf('<span class="%1$s">Posted on</span> %2$s <span class="meta-sep">by</span> %3$s',
-		'meta-prep meta-prep-author',
-		sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><span class="entry-date">%3$s</span></a>',
-			get_permalink(),
-			esc_attr( get_the_time() ),
-			get_the_date()
-		),
-		sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s">%3$s</a></span>',
-			get_author_posts_url( get_the_author_meta( 'ID' ) ),
-			sprintf( esc_attr( 'View all posts by %s'), get_the_author() ),
-			get_the_author()
-		)
-	);
-}
+    function hackeryou_posted_on() {
+        printf('<span class="%1$s">Posted on</span> %2$s <span class="meta-sep">by</span> %3$s',
+               'meta-prep meta-prep-author',
+               sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><span class="entry-date">%3$s</span></a>',
+                        get_permalink(),
+                        esc_attr( get_the_time() ),
+                        get_the_date()
+               ),
+               sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s">%3$s</a></span>',
+                        get_author_posts_url( get_the_author_meta( 'ID' ) ),
+                        sprintf( esc_attr( 'View all posts by %s'), get_the_author() ),
+                        get_the_author()
+               )
+        );
+    }
 endif;
 
 if ( ! function_exists( 'hackeryou_posted_in' ) ) :
 /**
  * Prints HTML with meta information for the current post (category, tags and permalink).
  */
-function hackeryou_posted_in() {
-	// Retrieves tag list of current post, separated by commas.
-	$tag_list = get_the_tag_list( '', ', ' );
-	if ( $tag_list ) {
-		$posted_in = 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.';
-	} elseif ( is_object_in_taxonomy( get_post_type(), 'category' ) ) {
-		$posted_in = 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.';
-	} else {
-		$posted_in = 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.';
-	}
-	// Prints the string, replacing the placeholders.
-	printf(
-		$posted_in,
-		get_the_category_list( ', ' ),
-		$tag_list,
-		get_permalink(),
-		the_title_attribute( 'echo=0' )
-	);
-}
+    function hackeryou_posted_in() {
+        // Retrieves tag list of current post, separated by commas.
+        $tag_list = get_the_tag_list( '', ', ' );
+        if ( $tag_list ) {
+            $posted_in = 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.';
+        } elseif ( is_object_in_taxonomy( get_post_type(), 'category' ) ) {
+            $posted_in = 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.';
+        } else {
+            $posted_in = 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.';
+        }
+        // Prints the string, replacing the placeholders.
+        printf(
+            $posted_in,
+            get_the_category_list( ', ' ),
+            $tag_list,
+            get_permalink(),
+            the_title_attribute( 'echo=0' )
+        );
+    }
 endif;
 
 /* Get rid of junk! - Gets rid of all the crap in the header that you dont need */
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-$(function(){
-	//your jQuery here
-});
+(function($) {
+    $(function() {
+	
+    });
+})(jQuery);
