a8231278e76c0956671d343553fa210f732a2ff2
Frank Wang <frank.wangtao@gmail.com>
2014-11-02 09:14:47 -0500

    Remove unnecessary ipsum text

--- home.php
+++ home.php
@@ -1,52 +1,49 @@
 <?php
 
 /*
-	Template Name: Home Page
-*/
+   Template Name: Home Page
+ */
 
 get_header();  ?>
 
 <div class="main">
 
-<div class="hero">
+  <div class="hero">
+    <div class="container">
+      <div class="form-booking">
+	<form>
+	  <h3>Book Now</h3>
+	  <input type="text" name="firstname" value="First Name"><br>
+	  <input type="text" name="lastname" value="Last Name">
+	  <input type="date" name="checkin" value="Checkin Date">
+	  <input type="date" name="checkout" value="Checkout Date">
+	  <!--       <input type="" name="roomtype" value="Select Room Type"> -->
+	  <input type="number" min="1" max="10" name="roomnumber" value="1">
+	  <input type="submit" value="Check Availability" class="submit"> 
+	</form>
+      </div> <!-- .form-booking -->
+    </div> <!-- /.container -->
+  </div> <!-- .hero -->
+
   <div class="container">
-  <div class="form-booking">
-    <form>
-      <h3>Book Now</h3>
-      <input type="text" name="firstname" value="First Name"><br>
-      <input type="text" name="lastname" value="Last Name">
-      <input type="date" name="checkin" value="Checkin Date">
-      <input type="date" name="checkout" value="Checkout Date">
-<!--       <input type="" name="roomtype" value="Select Room Type"> -->
-      <input type="number" min="1" max="10" name="roomnumber" value="1">
-      <input type="submit" value="Check Availability" class="submit"> 
-    </form>
-  </div> <!-- .form-booking -->
-  </div> <!-- /.container -->
-</div> <!-- .hero -->
+    <div class="wrapper clearfix">
+      <div class="front-page-rooms">
+	<img src="" alt="Image 1">
+      </div>
+      <div class="front-page-rooms">
+	<img src="" alt="Image 1">
+      </div>
+      <div class="front-page-rooms">
+	<img src="" alt="Image 1">
+      </div> <!-- .wrapper -->
+    </div>
+    
+    <div class="content-front">
+      <?php get_template_part( 'loop', 'home' ); ?>
+    </div> <!-- /.content-front -->
 
-<div class="container">
-<div class="wrapper clearfix">
-  <div class="front-page-rooms">
-    <img src="" alt="Image 1">
+
   </div>
-  <div class="front-page-rooms">
-    <img src="" alt="Image 1">
-  </div>
-  <div class="front-page-rooms">
-    <img src="" alt="Image 1">
-  </div> <!-- .wrapper -->
-  </div>
-  
-  <div class="content-front">
-
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda ullam, officiis sapiente dolore dolor tempore possimus iusto suscipit in mollitia! Laborum dolorum qui delectus deleniti, magni, nostrum ut dignissimos sunt.
-
-    <?php get_template_part( 'loop', 'home' ); ?>
-  </div> <!-- /.content-front -->
-
-
-</div>
 
 </div> <!-- /.main -->
 
