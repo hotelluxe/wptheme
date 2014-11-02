27c698907d118ba17144a40fbf191428c6c657f7
Frank Wang <frank.wangtao@gmail.com>
2014-11-02 09:13:02 -0500

    Remove unnecessary loop

--- page.php
+++ page.php
@@ -4,15 +4,13 @@
   <div class="container">
 
     <div class="content">
-      <?php // Start the loop ?>
-      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-
-      <?php endwhile; // end the loop?>
+      
+      <?php if (have_posts() ) : the_post(); ?>
+      <h2><?php the_title(); ?></h2>
+      <?php the_content(); ?>
+      <?php endif; ?>
     </div> <!-- /,content -->
-
+    
     <?php get_sidebar(); ?>
 
   </div> <!-- /.container -->
